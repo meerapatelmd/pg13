33b02130571bd7ea882bf2cba94ed69e5dee9cbd
Meera Patel <patelm9@mskcc.org>
2020-07-22 14:08:03 -0400

    add appendTable function

---
+++ R/appendTable.R
@@ -0,0 +1,27 @@
+#' Append to am Existing Table
+#' @import DatabaseConnector
+#' @description Like the writeTable function, this function is a wrapper around a DatabaseConnector function rather than one where a SQL statement is rendered using the SqlRender package. This function performs the additional step of converting all inputs to the data.frame class, especially in cases where the input is a tibble.
+#' @param tableName Name of table to write to.
+#' @param schema schema where `tableName` is located.
+#' @param .data dataframe to append
+#' @param ... Additional arguments passed to DatabaseConnector::dbAppendTable
+#' @export
+
+
+appendTable <-
+    function(conn = conn,
+             schema,
+             tableName,
+             .data,
+             ...) {
+
+            schemaTableName <- constructSchemaTableName(schema = schema,
+                                                        tableName = tableName)
+
+            DatabaseConnector::dbAppendTable(conn = conn,
+                                            name = schemaTableName,
+                                            value = .data %>%
+                                                as.data.frame(),
+                                            ...)
+
+    }
