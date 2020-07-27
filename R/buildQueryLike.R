15091d218fdedd994f9112a9b4210c10054cf6dc
Meera Patel <patelm9@mskcc.org>
2020-07-26 20:55:13 -0400

    add buildQueryLike function

---
+++ R/buildQueryLike.R
@@ -0,0 +1,43 @@
+#' Writes a Like SQL Query
+#' @return a SQL Query as a character string.
+#' @export
+
+buildQueryLike <-
+        function(fields = "*",
+                 distinct = FALSE,
+                 schema,
+                 tableName,
+                 whereLikeField,
+                 whereLikeValue,
+                 caseInsensitive = TRUE,
+                 limit_n = NULL) {
+
+
+                sql_construct <-
+                constructBase(fields = fields,
+                              distinct = distinct,
+                              schema = schema,
+                              tableName = tableName)
+
+                if (caseInsensitive) {
+                        sql_construct <-
+                                c(sql_construct,
+                                  constructWhereLowerLike(field = whereLikeField,
+                                                          term = tolower(whereLikeValue))
+                                ) %>%
+                                paste(collapse = " ")
+                } else {
+                        sql_construct <-
+                                c(sql_construct,
+                                  constructWhereLike(field = whereLikeField,
+                                                          term = whereLikeValue)
+                                ) %>%
+                                paste(collapse = " ")
+
+                }
+                sql_construct %>%
+                    terminateBuild()
+
+
+        }
+
