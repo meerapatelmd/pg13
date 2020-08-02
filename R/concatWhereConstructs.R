5b4f808d345af9c316e05bed0e0f7bdb99d8931f
Meera Patel <patelm9@mskcc.org>
2020-08-02 15:26:30 -0400

    add multiple WHERE construct feature

--- R/concatWhereConstructs.R
+++ R/concatWhereConstructs.R
@@ -1,15 +1,60 @@
 #' Concatenate 2 WHERE constructs
 #' @description When 2 WHERE constructs are included in the SQL Statement, they are concatenated and the 2nd "WHERE" is replaced with an "AND".
+#' @param AND If TRUE, the WHERE constructs are concatenated with "AND". If FALSE, the concatenation is performed with an "OR".
 #' @import stringr
 #' @export
 
 concatWhereConstructs <-
-    function(sql_construct, where_sql_construct_2) {
+    function(sql_construct,
+             where_sql_construct_2,
+             ...,
+             AND = TRUE) {
 
-                    c(sql_construct,
-                      stringr::str_replace(where_sql_construct_2,
-                                           "WHERE",
-                                           "AND")) %>%
-                                paste(collapse = " ")
+            if (missing(...)) {
+
+                            if (AND) {
+
+                                    c(sql_construct,
+                                      stringr::str_replace(where_sql_construct_2,
+                                                           "WHERE",
+                                                           "AND")) %>%
+                                                paste(collapse = " ")
+                            } else {
+
+                                    c(sql_construct,
+                                      stringr::str_replace(where_sql_construct_2,
+                                                           "WHERE",
+                                                           "OR")) %>%
+                                            paste(collapse = " ")
+
+                            }
+            } else {
+
+                    Args <- list(where_sql_construct_2,
+                                 ...) %>%
+                                unlist()
+
+                    if (AND) {
+
+                            c(sql_construct,
+                            stringr::str_replace_all(Args,
+                                                     "WHERE",
+                                                     "AND")) %>%
+                                    paste(collapse = " ")
+
+
+                    } else {
+
+                            c(sql_construct,
+                              stringr::str_replace_all(Args,
+                                                       "WHERE",
+                                                       "OR")) %>%
+                                    paste(collapse = " ")
+
+
+
+                    }
+
+            }
 
     }
---
+++ R/concatWhereIn.R
@@ -0,0 +1,25 @@
+#' Render "WHERE x IN y" Component
+#' @description This is a non-terminal render, meaning that the SQL component will not be terminated with a semicolon in order to construct complex SQL queries.
+#' @import SqlRender
+#' @param field Single field to be filtered for
+#' @param vector vector of values that the SQL query is filtering for
+#' @export
+
+
+constructWhereIn <-
+    function(field,
+             vector) {
+
+        if (is.character(vector)) {
+
+                vector <- paste0("'", vector, "'")
+
+        }
+
+        base <- system.file(package='pg13')
+        path <- paste0(base, "/sql_constr")
+
+        SqlRender::render(SqlRender::readSql(paste0(path, "/whereIn.sql")),
+                          field = field,
+                          vector = vector)
+    }
