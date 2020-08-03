7c1aac618d7f8d221b33031bc800bd5c51434abd
Meera Patel <patelmeeray@gmail.com>
2020-08-02 21:56:33 -0400

    add sourceFilePath function

--- R/sourceFilePath.R
+++ R/sourceFilePath.R
@@ -1,5 +1,5 @@
 #' Get SourceFile Path
-#' @desciption This function provides the path for files installed within a given package's library.
+#' @description This function provides the path for files installed within a given package's library.
 #' @param instSubdir Name of subdirectory in the inst/ folder
 #' @param FileName Name of file in subdirectory
 #' @param package Package name
