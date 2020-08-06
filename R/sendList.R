caf84ce914a1e8122beaded0df8c2a12c5ff2a83
Meera Patel <patelm9@mskcc.org>
2020-08-06 19:21:47 -0400

    add error feature to sendList function

--- R/sendList.R
+++ R/sendList.R
@@ -4,6 +4,10 @@
 #' @import progress
 #' @param conn Connection object
 #' @param sqlList list object of queries
+#' @param verbose If TRUE, the SQL is printed back before executing
+#' @param progressBar If TRUE, a progress bar is returned in the console.
+#' @param skipErrors If TRUE, if a SQL execution returns an error, the statement is printed back in red in the console and the iteration will proceed to the next sql statement in line.
+#' @param errorFile (optional) path to the error file where any errors are written to if not null.
 #' @param ... Additional arguments to pass onto the DatabaseConnector::dbGetQuery function
 #' @export
 
@@ -12,6 +16,8 @@ sendList <-
              sqlList,
              verbose = TRUE,
              progressBar = TRUE,
+             skipErrors = TRUE,
+             errorFile = NULL,
              ...) {
 
             if (!is.list(sqlList)) {
@@ -35,7 +41,7 @@ sendList <-
                     sql <- sqlList[[i]]
 
                     if (verbose) {
-                            secretary::typewrite(sql)
+                            secretary::typewrite("\n", sql)
                     }
 
                     if (progressBar) {
@@ -43,9 +49,28 @@ sendList <-
                             Sys.sleep(.2)
                     }
 
-                    send(conn = conn,
-                          sql_statement = sql,
-                          ...)
+
+                    if (skipErrors) {
+
+                            tryCatch(send(conn = conn,
+                                        sql_statement = sql,
+                                        ...),
+                                     error = function(err) {
+                                             secretary::typewrite_error("\n", sql)
+                                             if (!is.null(errorFile)) {
+                                                     .writeErrorReport(message = err$message,
+                                                                       sql = sql,
+                                                                       errorFile = errorFile)
+                                             }
+                                     })
+
+
+                    } else {
+
+                            send(conn = conn,
+                                 sql_statement = sql,
+                                 ...)
+                    }
 
             }
     }
