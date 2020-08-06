433f8d6d620562d98d14669ca15d294115d9d6a6
Meera Patel <patelm9@mskcc.org>
2020-08-06 18:31:31 -0400

    bug fix parseSQL function

--- R/parseSQL.R
+++ R/parseSQL.R
@@ -2,6 +2,8 @@
 #' @description
 #' This function is helpful in splitting a SQL file into digestible smaller statements based on the semicolon for troubleshooting or to skip of the statements that do not work within the full input SQL.
 #' @import centipede
+#' @return
+#' List of sql statements
 #' @export
 
 parseSQL <-
@@ -9,5 +11,6 @@ parseSQL <-
                 centipede::strsplit(sql_statement, split = "[\\;]{1}", type = "after") %>%
                         unlist() %>%
                         trimws() %>%
-                        centipede::no_blank()
+                        centipede::no_blank() %>%
+                        as.list()
         }
