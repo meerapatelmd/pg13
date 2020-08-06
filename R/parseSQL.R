83ba1d1f9b334d05617736e496c77060d5ac09b2
Meera Patel <patelm9@mskcc.org>
2020-08-06 17:25:44 -0400

    closes #4

---
+++ R/parseSQL.R
@@ -0,0 +1,13 @@
+#' Parse SQL into Single Statements
+#' @description
+#' This function is helpful in splitting a SQL file into digestible smaller statements based on the semicolon for troubleshooting or to skip of the statements that do not work within the full input SQL.
+#' @import centipede
+#' @export
+
+parseSQL <-
+        function(sql_statement) {
+                centipede::strsplit(sql_statement, split = "[\\;]{1}", type = "after") %>%
+                        unlist() %>%
+                        trimws() %>%
+                        centipede::no_blank()
+        }
