a008116c29c61a34a6dd7bbe5a4b0bc62ed6c91d
Meera Patel <patelm9@mskcc.org>
2020-08-28 15:55:34 -0400

    add renderGrantSchema function

---
+++ R/renderGrantSchema.R
@@ -0,0 +1,33 @@
+#' @title
+#' Render GRANT ALL PRIVILEGES
+#' @description
+#' Render SQL to grant all privileges to a schema a user or a group
+#' @param schema schema to grant privileges to
+#' @param group group name, Default: NULL
+#' @param user user name, Default: NULL
+#' @seealso
+#'  \code{\link[SqlRender]{render}},\code{\link[SqlRender]{readSql}}
+#' @rdname renderGrantSchema
+#' @export
+#' @importFrom SqlRender render readSql
+
+renderGrantSchema <-
+        function(schema,
+                 group = NULL,
+                 user = NULL) {
+
+                if (is.null(group) && is.null(user)) {
+                        stop("group or user is required")
+                }
+
+                if (!is.null(group)) {
+                        SqlRender::render(SqlRender::readSql(paste0(path, "/grantGroup.sql")),
+                                          schema = schema,
+                                          gp = group)
+                } else {
+                        SqlRender::render(SqlRender::readSql(paste0(path, "/grantUser.sql")),
+                                          schema = schema,
+                                          user = user)
+                }
+
+        }
