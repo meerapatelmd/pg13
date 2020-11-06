bf0216ac5a73e88329636c2886f2d0350feb99a1
Meera <patelmeeray@gmail.com>
2020-11-06 17:41:19 -0500

    bug fix

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onLoad <-
         function(lib,pkg) {
-                safely.show <<- purrr::quietly(methods::show)
+                safely.print <<- purrr::quietly(print)
                 is_conn_open <<-
                         function(conn, verbose = TRUE) {
 
-                                output <- safely.show(conn)
+                                output <- safely.print(conn)
 
                                 if (is.null(output$error)) {
                                         TRUE
