de9a087bfaa19d6e2d30ea2005069eacedc3a1e4
Meera <patelmeeray@gmail.com>
2020-11-06 17:55:12 -0500

    add dummy zzz functions

---
+++ R/zzz-utils.R
@@ -0,0 +1,23 @@
+#' Print an Object with \code\link{[purrr]{safely}} Wrapper
+#' @noRd
+
+safely.print <-
+        function() {
+                "dummy"
+        }
+
+
+#' @title
+#' Is the Connection Open?
+#'
+#' @description
+#' Check if a Connection Class object is open.
+#'
+#' @param conn Connection object
+#'
+#' @export
+
+is_conn_open <-
+        function(conn) {
+                "dummy"
+        }
--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,14 @@
 .onLoad <-
         function(lib,pkg) {
 
-                safely.print <<- purrr::safely(print)
+                safely.print <<- purrr::safely(print,
+                                               quiet = TRUE)
+
 
                 is_conn_open <<-
                         function(conn) {
 
-                                output <- safely.print(conn)
+                                output <- suppressMessages(safely.print(conn))
 
                                 if (is.null(output$error)) {
                                         TRUE
