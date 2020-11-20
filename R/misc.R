feaa555074c231852e89cd349a36cf79aa5f2177
Meera <patelm9@mskcc.org>
2020-11-19 20:21:05 -0500

    update newest version

--- R/misc.R
+++ R/misc.R
@@ -50,3 +50,69 @@ fileSend <-
 
 
 
+
+
+
+
+
+#' Query using a link to SQL file
+#' @import DatabaseConnector
+#' @import SqlRender
+#' @export
+
+linkQuery <-
+    function(conn,
+             link) {
+
+                tmp_sql <- tempfile(fileext = ".txt")
+
+                download.file(link,
+                              destfile = tmp_sql)
+
+
+
+                query(conn = conn,
+                      statement = SqlRender::readSql(tmp_sql))
+
+
+                unlink(tmp_sql)
+
+    }
+
+
+
+
+
+#' Send a statument using a link to SQL file
+#' @import DatabaseConnector
+#' @import SqlRender
+#' @export
+
+linkSend <-
+    function(conn,
+             link) {
+
+                tmp_sql <- tempfile(fileext = ".txt")
+
+                download.file(link,
+                              destfile = tmp_sql)
+
+
+
+                send(conn = conn,
+                      statement = SqlRender::readSql(tmp_sql))
+
+
+                unlink(tmp_sql)
+
+    }
+
+
+
+
+
+
+
+
+
+
