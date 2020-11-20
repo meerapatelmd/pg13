2d4135427b5bb740a5d30d50358170a75fdfc2b7
Meera <patelm9@mskcc.org>
2020-11-19 19:45:43 -0500

    deprecate internal functions

--- R/deprec.R
+++ R/deprec.R
@@ -684,3 +684,34 @@ rmIfClosed <-
         }
 
     }
+
+
+#' Get SourceFile Path
+#' @description This function provides the path for files installed within a given package's library.
+#' @param instSubdir Name of subdirectory in the inst/ folder
+#' @param FileName Name of file in subdirectory
+#' @param package Package name
+#' @noRd
+
+
+sourceFilePath <-
+    function(instSubdir,
+             FileName,
+             package) {
+
+        .Deprecated()
+        paste0(system.file(package = package), "/", instSubdir, "/", FileName)
+    }
+
+#' Terminate a SQL Statement with a semicolon
+#' @noRd
+
+terminateBuild <-
+    function(sql_statement) {
+
+        .Deprecated()
+
+        paste0(sql_statement, ";")
+
+    }
+
