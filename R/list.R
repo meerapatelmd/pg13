e398377d0d12b501da421efcfe45c51806e4d474
Meera Patel <patelmeeray@gmail.com>
2020-12-25 16:16:33 -0500

    move ls_users

--- R/list.R
+++ R/list.R
@@ -158,3 +158,43 @@ ls_tables <-
 
     }
 
+#' @title
+#' List Users
+#' @rdname ls_users
+#' @export
+#' @family list functions
+
+ls_users <-
+    function(conn,
+             conn_fun,
+             verbose = TRUE,
+             render_sql = TRUE,
+             render_only = FALSE,
+             ...) {
+
+        sql_statement <-
+            "SELECT usename AS role_name,
+  CASE
+     WHEN usesuper AND usecreatedb THEN
+	   CAST('superuser, create database' AS pg_catalog.text)
+     WHEN usesuper THEN
+	    CAST('superuser' AS pg_catalog.text)
+     WHEN usecreatedb THEN
+	    CAST('create database' AS pg_catalog.text)
+     ELSE
+	    CAST('' AS pg_catalog.text)
+  END role_attributes
+FROM pg_catalog.pg_user
+ORDER BY role_name desc;"
+
+        query(conn = conn,
+              conn_fun = conn_fun,
+              sql_statement = sql_statement,
+              verbose = verbose,
+              render_sql = render_sql,
+              render_only = render_only,
+              ...)
+
+
+    }
+
