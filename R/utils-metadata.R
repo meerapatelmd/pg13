2a5a6e984197a41bdbfede9f84f8fa66ca68ee45
Meera Patel <patelmeeray@gmail.com>
2020-12-25 16:23:25 -0500

    combine metadata utils

--- R/utils-metadata.R
+++ R/utils-metadata.R
@@ -1,3 +1,22 @@
+#' @title
+#' Get Connection Database
+#'
+#' @description
+#' Get the name of the database that the connection object is connected to.
+#'
+#' @export
+#' @rdname get_conn_db
+
+get_conn_db <-
+        function(conn) {
+                conn@jConnection$getCatalog()
+
+        }
+
+
+
+
+
 #' @export
 #' @rdname query_field_type
 #' @title
@@ -37,3 +56,8 @@ query_field_type <-
                       ... )
 
         }
+
+
+
+
+
