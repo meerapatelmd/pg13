4bca49d1faa67eb9e084ab3d0ce3d90f3a160536
Meera Patel <patelmeeray@gmail.com>
2020-12-25 13:04:18 -0500

    restore summary functions with example

--- R/summary.R
+++ R/summary.R
@@ -1,204 +1,189 @@
-
-
-
-summarize_table <-
+#' @title
+#' Summarize the Counts and Values of a Field
+#'
+#' @description
+#' \code{\link{summarize_field}} gets the total rows, distinct value count, valueset, and null counts for a single field in a table. To get the summary of more than 1 field, see \code{\link{summarize_fields}}. To summarize an entire table or schema without hand-selecting fields, see \code{\link{summarize_table}} and \code{\link{summarize_schema}} respectively.
+#'
+#'
+#'
+#' @rdname summarize_field
+#' @export
+#' @example inst/example/summary.R
+summarize_field <-
         function(conn,
+                 conn_fun,
                  schema,
                  table,
+                 field,
                  verbose = TRUE,
                  render_sql = TRUE,
-                 warn_no_rows = TRUE,
                  render_only = FALSE,
+                 warn_no_rows = TRUE,
                  ...) {
 
-                # schema <- "test_schema"
-                # table <- "test_table"
 
-                fields <-
-                        ls_fields(conn = conn,
-                                  schema = schema,
-                                  table = table,
-                                  verbose = verbose,
-                                  render_sql = render_sql)
-
-                table_rows <-
-                        query(
+                field <- tolower(field)
+                if (!field_exists(
                                 conn = conn,
-                                sql_statement =
-                                        SqlRender::render("SELECT COUNT(*)\nFROM @schema.@tableName;",
-                                                          schema = schema,
-                                                          fields = fields,
-                                                          tableName = table),
-                                verbose = verbose,
-                                render_sql = render_sql,
-                                render_only = render_only,
-                                warn_no_rows = warn_no_rows,
-                                ...)
+                                schema = schema,
+                                table = table,
+                                field = field)) {
+
+                        stop(sprintf("field '%s' does not exist in %s.%s", field, schema, table))
+
+                }
+
+                sql_statements <-
+                        list(TOTAL_ROWS = sprintf("SELECT COUNT(*) FROM %s.%s;", schema, table),
+                                DISTINCT_VALUE_COUNT = sprintf("WITH valueset AS (SELECT DISTINCT %s FROM %s.%s) SELECT COUNT(*) FROM valueset;", field, schema, table),
+                             VALUESET = sprintf("SELECT DISTINCT %s FROM %s.%s;", field, schema, table),
+                             NULL_COUNT = sprintf("SELECT COUNT(%s) FROM %s.%s WHERE %s IS NULL;", field, schema, table, field)
+                             )
+
+                output <- list()
+                for (i in seq_along(sql_statements)) {
+
+                        sql_statement <- sql_statements[[i]]
+
+                        output[[i]] <-
+                                query(conn = conn,
+                                      conn_fun = conn_fun,
+                                      sql_statement = sql_statement,
+                                      verbose = verbose,
+                                      render_sql = render_sql,
+                                      warn_no_rows = warn_no_rows,
+                                      render_only = render_only,
+                                      ...)
+
+                        names(output)[i] <- names(sql_statements)[i]
+
+                }
+
+                output
+
+        }
 
 
-                output2 <- list()
+#' @title
+#' Summarize 1 or more Fields
+#' @inherit summarize_field description
+#' @rdname summarize_fields
+#' @export
+#' @example inst/example/summary.R
+
+summarize_fields <-
+        function(conn,
+                 conn_fun,
+                 schema,
+                 table,
+                 fields,
+                 verbose = TRUE,
+                 render_sql = TRUE,
+                 render_only = FALSE,
+                 warn_no_rows = TRUE,
+                 ...) {
+
+                output <- list()
                 for (field in fields) {
 
-
-                        sql_01_distinct_value_ct <-
-                                SqlRender::render("WITH a AS (SELECT DISTINCT @field FROM @schema.@table)\nSELECT COUNT(*) FROM a;", field = field, schema = schema, table = table)
-
-
-                        sql_02_distinct_values <-
-                                SqlRender::render("SELECT DISTINCT @field FROM @schema.@table;", field = field, schema = schema, table = table)
-
-                        sql_03_missingness_null <-
-                                SqlRender::render("SELECT COUNT(@field) FROM @schema.@table WHERE @field IS NULL;", field = field, schema = schema, table = table)
-
-
-                        sql_04_missingness_na <-
-                                SqlRender::render("SELECT COUNT(@field) FROM @schema.@table WHERE @field IN ('NA');", field = field, schema = schema, table = table)
-
-                        sql_statements <-
-                                c(sql_01_distinct_value_ct,
-                                  sql_02_distinct_values,
-                                  sql_03_missingness_null,
-                                  sql_04_missingness_na)
-
-                        names(sql_statements) <-
-                                c("DISTINCT_COUNT",
-                                  "VALUESET",
-                                  "NULL_COUNT",
-                                  "NA_COUNT")
-
-
-                        output <- list()
-                        for (sql_statement in sql_statements) {
-
-                                output[length(output)+1] <-
-                                        query(
-                                                conn = conn,
-                                                sql_statement = sql_statement,
+                        output[[length(output)+1]] <-
+                                summarize_field(conn = conn,
+                                                conn_fun = conn_fun,
+                                                schema = schema,
+                                                table = table,
+                                                field = field,
                                                 verbose = verbose,
                                                 render_sql = render_sql,
                                                 render_only = render_only,
                                                 warn_no_rows = warn_no_rows,
                                                 ...)
 
-
-                                names(output)[length(output)] <- names(sql_statement)
-
-                        }
-
-
+                        names(output)[length(output)] <- field
 
                 }
 
+                output
+
         }
-
-
 
 
 #' @title
-#' Summarize a Schema's Row Counts
-#' @rdname nrow_schema
+#' Summarize all Fields in a Table
+#' @inherit summarize_field description
+#' @example inst/example/summary.R
+#' @rdname summarize_table
 #' @export
-#'
-#' @importFrom purrr map
-#' @importFrom tibble as_tibble_col
-#' @importFrom dplyr bind_rows mutate
+summarize_table <-
+        function(
+                conn,
+                conn_fun,
+                schema,
+                table,
+                verbose = TRUE,
+                render_sql = TRUE,
+                render_only = FALSE,
+                warn_no_rows = TRUE,
+                ...
+        ) {
 
-nrow_schema <-
-        function(conn,
-                 schema,
-                 verbose = TRUE,
-                 render_sql = TRUE) {
+                fields <- ls_fields(conn = conn,
+                                    schema = schema,
+                                    table = table,
+                                    verbose = verbose,
+                                    render_sql = render_sql)
 
-                tables <- ls_tables(conn = conn,
-                                    schema = schema)
-
-
-
-                output <- list()
-
-                for (table in tables) {
-
-                        fields <-
-                                ls_fields(conn = conn,
-                                          schema = schema,
-                                          table = table,
-                                          verbose = verbose,
-                                          render_sql = render_sql)
-
-                        sql_01_table_rows <-
-                                SqlRender::render("SELECT COUNT(@fields)\nFROM @schema.@tableName;",
-                                                  schema = schema,
-                                                  fields = fields,
-                                                  tableName = table)
-
-
-                        for (field in fields) {
-
-                                sql_02_distinct_value_ct <-
-                                        SqlRender::render("SELECT DISTINCT COUNT")
-
-
-
-
-
-
-                        }
-
-                }
-
-                output <- list()
-                for (i in (length(output)+1):length(tables)) {
-
-                        table <- tables[i]
-
-                        table_cols <-
-                                query(conn = conn,
-                                      sql_statement = buildQuery(schema = schema,
-                                                                 tableName = table,
-                                                                 n = 1,
-                                                                 n_type = "limit")) %>%
-                                colnames()
-
-                        total_rows <-
-                                query(conn = conn,
-                                      sql_statement = renderRowCount(
-                                              schema = schema,
-                                              tableName = table)) %>%
-                                unlist()
-
-                        output_j <- list()
-                        for (j in 1:length(tableCols)) {
-                                table_col <- table_cols[j]
-
-                                output_j[[j]] <-
-                                        query(conn = conn,
-                                              sql_statement = buildQuery(fields = tableCol,
-                                                                         distinct = TRUE,
-                                                                         schema = schema,
-                                                                         tableName = Table)) %>%
-                                        nrow()
-
-                                names(output_j)[j] <- table_col
-
-                                Sys.sleep(1)
-
-                        }
-
-                        output[[i]] <-
-                                output_j  %>%
-                                purrr::map(tibble::as_tibble_col, "DISTINCT_COUNT") %>%
-                                dplyr::bind_rows(.id = "FIELD") %>%
-                                dplyr::mutate(total_rows = total_rows)
-
-                        names(output)[i] <- table
-
-                }
-
-                dplyr::bind_rows(output,
-                                 .id = "TABLE")
+                summarize_fields(conn = conn,
+                                 conn_fun = conn_fun,
+                                 schema = schema,
+                                 table = table,
+                                 fields = fields,
+                                 verbose = verbose,
+                                 render_sql = render_sql,
+                                 render_only = render_only,
+                                 warn_no_rows = warn_no_rows,
+                                 ...)
         }
 
 
+#' @title
+#' Summarize all Fields in a Schema
+#' @inherit summarize_field description
+#' @example inst/example/summary.R
+#' @rdname summarize_schema
+#' @export
+summarize_schema <-
+        function( conn,
+                  conn_fun,
+                  schema,
+                  verbose = TRUE,
+                  render_sql = TRUE,
+                  render_only = FALSE,
+                  warn_no_rows = TRUE,
+                  ...) {
 
 
+                tables <- ls_tables(conn = conn,
+                                    schema = schema,
+                                    verbose = verbose,
+                                    render_sql = render_sql)
 
+                output <- list()
+                for (i in seq_along(tables)) {
+
+                        table <- tables[i]
+
+                        output[[i]] <-
+                                summarize_table(conn = conn,
+                                                schema = schema,
+                                                table = table,
+                                                verbose = verbose,
+                                                render_sql = render_sql,
+                                                render_only = render_only,
+                                                warn_no_rows = warn_no_rows,
+                                                ...)
+
+                        names(output)[i] <- table
+                }
+
+                output
+        }
