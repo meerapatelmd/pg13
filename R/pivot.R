69775e3519131985aa0d6d6a80c7d9015351c81f
Meera Patel <patelmeeray@gmail.com>
2020-12-27 15:41:04 -0500

    add pivot_table function

---
+++ R/pivot.R
@@ -0,0 +1,90 @@
+#' @title
+#' Pivot a Table
+#'
+#' @rdname pivot_label
+#' @example inst/example/pivot.R
+#' @export
+
+pivot_table <-
+        function(conn,
+                 conn_fun,
+                 schema,
+                 table,
+                 id_column,
+                 names_from_column,
+                 values_from_column,
+                 verbose = TRUE,
+                 render_sql = TRUE,
+                 render_only = FALSE
+                 ) {
+
+
+                # Get `names_from` values
+                new_field_names <-
+                query(conn = conn,
+                      conn_fun = conn_fun,
+                      sql_statement =
+                                SqlRender::render(
+                                        "
+                                        SELECT DISTINCT @names_from_column
+                                        FROM @schema.@table
+                                        ",
+                                        names_from_column = names_from_column,
+                                        schema = schema,
+                                        table = table
+                                ),
+                      verbose = verbose,
+                      render_sql = render_sql,
+                      render_only = render_only) %>%
+                        unlist() %>%
+                        sort()
+
+
+                # <---! Value Type of the "Valuees From" column
+                values_from_data_type <-
+                        query(conn = conn,
+                              conn_fun = conn_fun,
+                                sql_statement =
+                                        SqlRender::render(
+                                                "
+                                                SELECT
+                                                        udt_name as data_type
+                                                FROM information_schema.columns
+                                                WHERE table_name = '@tableName'
+                                                        AND column_name = '@values_from_column'
+                                                ",
+                                                tableName = table,
+                                                values_from_column = values_from_column),
+                              verbose = verbose,
+                              render_sql = render_sql,
+                              render_only = render_only) %>%
+                        unlist()
+
+
+                new_field_names_ddl <-
+                        sprintf("%s %s", new_field_names, values_from_data_type)
+
+
+
+                sql_statement <-
+                        SqlRender::render(
+                        "
+                        SELECT *
+                                FROM crosstab( 'select @id_column, @names_from_column, @values_from_column from @schema.@table ORDER BY 1,2')
+                        AS final_result(@id_column VARCHAR, @new_field_names_ddl)
+                        ",
+                        id_column = id_column,
+                        new_field_names_ddl = new_field_names_ddl,
+                        values_from_column = values_from_column,
+                        names_from_column = names_from_column,
+                        schema = schema,
+                        table = table
+                        )
+
+                query(conn = conn,
+                      conn_fun = conn_fun,
+                      sql_statement = sql_statement,
+                      verbose = verbose,
+                      render_sql = render_sql,
+                      render_only = render_only)
+        }
---
+++ inst/example/pivot.R
@@ -0,0 +1,60 @@
+library(tidyverse)
+conn <- pg13::local_connect()
+ pg13::write_table(conn = conn,
+                  schema = "public",
+                  table_name = "test_table",
+                  drop_existing = TRUE,
+                  data =
+                          tribble(~Person,~Attribute, ~Attribute_Value,
+                                  "Meera", "Height", "Tall",
+                                  "Syed", "Height", "Tall",
+                                  "Syed", "Character", "Funny",
+                                  "Meera", "Character", NA_character_))
+
+pivot_table(conn = conn,
+            schema = "public",
+            table = "test_table",
+            id_column = "person",
+            names_from_column = "attribute",
+            values_from_column = "attribute_value")
+
+
+pg13::write_table(conn = conn,
+                  schema = "public",
+                  table_name = "test_table",
+                  drop_existing = TRUE,
+                  data =
+                          tribble(~Person,~Attribute, ~Attribute_Value, ~Attribute_Date,
+                                  "Meera", "Age", "Tall", as.Date("2020-12-27"),
+                                  "Syed", "Height", "Tall", as.Date("2020-12-27"),
+                                  "Syed", "Age", "Funny", as.Date("2020-12-27")))
+pivot_table(conn = conn,
+            schema = "public",
+            table = "test_table",
+            id_column = "person",
+            names_from_column = "attribute",
+            values_from_column = "attribute_date")
+
+
+pg13::write_table(conn = conn,
+                  schema = "public",
+                  table_name = "test_table",
+                  drop_existing = TRUE,
+                  data =
+                          tribble(~Person,~Measurement, ~Results, ~Results_Date,
+                                  "Meera", "Age", 36, as.Date("2020-12-27"),
+                                  "Syed", "Height", 74, as.Date("2020-12-27"),
+                                  "Syed", "Age", 32, as.Date("2020-12-27"),
+                                  "Meera", "Height", 68, as.Date("2020-12-28")
+                                  )
+                                )
+
+pivot_table(conn = conn,
+            schema = "public",
+            table = "test_table",
+            id_column = "person",
+            names_from_column = "measurement",
+            values_from_column = "results")
+
+pg13::dc(conn = conn)
+
