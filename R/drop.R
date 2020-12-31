9d687e158aee7da733852a19246624741649cd7f
Meera Patel <patelmeeray@gmail.com>
2020-12-30 19:23:43 -0500

    add drop table batch funtion

--- R/drop.R
+++ R/drop.R
@@ -57,6 +57,101 @@ drop_table <-
         }
 
 
+
+#' @title
+#' Drop a Batch of Tables
+#'
+#' @inheritParams base_args
+#' @inheritParams base::grep
+#' @param           if_exists   If TRUE, the table will be dropped only if it exists.
+#' @param           ...         Additional arguments passed to `DatabaseConnector::dbSendStatement()`
+#'
+#' @rdname drop_table_batch
+#' @export
+#' @example inst/example/table.R
+#' @family table functions
+#' @family drop functions
+
+drop_table_batch <-
+        function(conn,
+                 conn_fun,
+                 schema,
+                 table,
+                 if_exists = TRUE,
+                 pattern,
+                 ignore.case = FALSE,
+                 perl = FALSE,
+                 fixed = FALSE,
+                 useBytes = FALSE,
+                 invert = FALSE,
+                 verbose = TRUE,
+                 render_sql = TRUE,
+                 render_only = FALSE,
+                 ...) {
+
+                tables <- ls_tables(conn = conn,
+                                  conn_fun = conn_fun,
+                                  schema = schema,
+                                  verbose = verbose,
+                                  render_sql = render_sql)
+
+                tables <-
+                        grep(pattern = pattern,
+                             x = tables,
+                             ignore.case = ignore.case,
+                             perl = perl,
+                             value = TRUE,
+                             fixed = fixed,
+                             useBytes = useBytes,
+                             invert = invert)
+
+
+
+                for (table in tables) {
+
+                        if (if_exists) {
+
+                                sql_statement <- sprintf("DROP TABLE IF EXISTS %s.%s;", schema, table)
+
+                        } else {
+
+                                sql_statement <- sprintf("DROP TABLE %s.%s;", schema, table)
+
+                        }
+
+
+                        if (verbose) {
+
+                                typewrite_activity(sprintf("Dropping %s.%s...", schema, table))
+
+
+                        }
+
+
+                        if (interactive()) {
+
+                                secretary::press_enter()
+                        }
+
+                        send(conn = conn,
+                             sql_statement = sql_statement,
+                             verbose = verbose,
+                             render_sql = render_sql,
+                             render_only = render_only,
+                             ...)
+
+
+                        if (verbose) {
+
+                                typewrite_activity(sprintf("Dropping %s.%s...complete", schema, table))
+
+                        }
+
+                }
+
+        }
+
+
 #' Drop a Postgres schema
 #' @description Drop a schema if it exists.
 #' @param ... Additional arguments passed to the DatabaseConnector::dbSendStatement function
