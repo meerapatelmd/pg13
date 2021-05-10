dc04840fe570930a423dee729e7af41ae11bab39
Meera <mpatel@concertai.com>
2021-05-10 12:51:25 -0400

    remove cli dependency, add chunk option to sql, change sql output color to blue

--- R/utils-typewrite.R
+++ R/utils-typewrite.R
@@ -1,20 +1,61 @@
 #' Typewrite SQL
-#' @importFrom secretary typewrite greenTxt
+#' @importFrom secretary typewrite blueTxt
 #' @importFrom stringr str_replace_all
 #' @noRd
 
 typewrite_sql <-
-        function (sql_statement)
+        function (sql_statement,
+                  style = c("inline", "chunk"))
         {
-                sql_statement <- stringr::str_replace_all(sql_statement,
-                                                          "[\r\n\t]{1,}|\\s{2,}", " ")
-                sql_statement <- trimws(sql_statement)
-                secretary::typewrite(secretary::greenTxt("SQL:"), sql_statement)
+
+                style <-
+                match.arg(style,
+                          choices = c("inline", "chunk"),
+                          several.ok = FALSE)
+
+                if (style %in% "inline") {
+
+                        # Flatten SQL Statement
+                        sql_statement <- stringr::str_replace_all(sql_statement,
+                                                                  "[\r\n\t]{1,}|\\s{2,}", " ")
+                        sql_statement <- trimws(sql_statement)
+
+                        secretary::typewrite(secretary::enbold(secretary::blueTxt("SQL:")),
+                                             sql_statement)
+
+                } else {
+
+                secretary::typewrite(secretary::enbold(secretary::blueTxt("SQL:")))
+                cat_sql_chunk(sql_statement = sql_statement,
+                              tab_count = 4)
+
+                }
+        }
+
+
+#' Cat SQL Chunk
+#' @importFrom secretary typewrite blueTxt
+#' @importFrom stringr str_replace_all
+#' @noRd
+
+cat_sql_chunk <-
+        function (sql_statement,
+                  tab_count = 4) {
+                tabs <- rep("\t", tab_count)
+                tabs <- paste(tabs, collapse = "")
+
+                output <-
+                  c(
+                        sprintf("%s```sql", tabs),
+                        sprintf("%s%s", tabs, sql_statement),
+                        sprintf("%s```", tabs)
+                  )
+
+                cat(output, sep = "\n")
         }
 
 #' Typewrite Activity
-#' @importFrom secretary typewrite greenTxt
-#' @importFrom stringr str_replace_all
+#' @importFrom secretary typewrite yellowTxt
 #' @noRd
 
 typewrite_activity <-
@@ -27,8 +68,9 @@ typewrite_activity <-
 
 typewrite_alert_danger <-
         function(text) {
-                cat(sprintf("[%s]", as.character(Sys.time())), "\t")
-                cli::cli_alert_danger(text = text)
+                text <-
+                        sprintf("! %s", text)
+                secretary::typewrite(secretary::redTxt(secretary::enbold(text)))
         }
 
 
@@ -36,6 +78,8 @@ typewrite_alert_danger <-
 
 typewrite_alert_success <-
         function(text) {
-                cat(sprintf("[%s]", as.character(Sys.time())), "\t")
-                cli::cli_alert_success(text = text)
+                text <-
+                        sprintf("\u2713 %s", text)
+
+                secretary::typewrite(secretary::greenTxt(secretary::enbold(text)))
         }
